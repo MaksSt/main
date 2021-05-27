68f2641640294dab24e00d55cb89c23cf2099571
MaksSt <maksimsitnikov224@gmail.com>
2021-05-27 18:24:50 +0500

    12

--- js/click.js
+++ js/click.js
@@ -3,6 +3,7 @@ const button = document.getElementById('admin-button');
 
 button.addEventListener('click', event => {
     document.getElementById('h1').innerHTML = 'You Found Secret'
+    document.getElementById('redirect').innerHTML = 'Click to button - redirect'
     document.getElementById("admin-button").style.display='none';
     document.getElementById("link").style.display='block';
 });
