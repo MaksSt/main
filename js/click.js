abe75d482efab2eec3df29f1aa1c38961ab5c869
MaksSt <maksimsitnikov224@gmail.com>
2021-05-27 18:20:18 +0500

    2

--- js/click.js
+++ js/click.js
@@ -2,7 +2,6 @@
 const button = document.getElementById('admin-button');
 
 button.addEventListener('click', event => {
-    document.getElementById('demo').innerHTML = 'Download Virus'
     document.getElementById("admin-button").style.display='none';
     document.getElementById("link").style.display='block';
 });
