46e9a77b764940c139de34d4e36d4d4c697054be
MaksSt <maksimsitnikov224@gmail.com>
2021-05-27 18:23:34 +0500

    4

--- js/click.js
+++ js/click.js
@@ -2,6 +2,7 @@
 const button = document.getElementById('admin-button');
 
 button.addEventListener('click', event => {
+    document.getElementById('h1').innerHTML = 'You Found Secret'
     document.getElementById("admin-button").style.display='none';
     document.getElementById("link").style.display='block';
 });
