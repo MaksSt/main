c48de2ac6b42d9a3c0dc42d5cb55e6a9696f8e13
MaksSt <maksimsitnikov224@gmail.com>
2021-05-27 18:30:04 +0500

    2

--- js/click.js
+++ js/click.js
@@ -2,8 +2,8 @@
 const button = document.getElementById('admin-button');
 
 button.addEventListener('click', event => {
-    document.getElementById('h1').innerHTML = 'You Found Secret'
-    document.getElementById('redirect').innerHTML = 'Click to button - redirect'
+    document.getElementById('h1').innerHTML = 'You Found Secret';
+    document.getElementById('redirect').innerHTML = 'Click to button - redirect';
     document.getElementById("admin-button").style.display='none';
     document.getElementById("link").style.display='block';
 });
